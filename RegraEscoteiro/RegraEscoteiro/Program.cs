957609916245230a41cc53287cf6ed1c009421d7
Delson Magalhaes <dl_magalhaes@hotmail.com>
2023-10-13 09:25:23 -0300

    Commit Inicial

---
+++ RegraEscoteiro/RegraEscoteiro/Program.cs
@@ -0,0 +1,18 @@
+﻿// Limpando informações desnecessárias
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Valor percentual: " + CalculaValorPercentual(120, 20));
+    }
+
+
+    // Nome da função mais entendivel
+    // Nome das variáveis mais entendiveis
+    public static double CalculaValorPercentual(double pValorBase, double pPercentual)
+    {
+        // Refatorando, diminuindo o código
+        return pValorBase * (pPercentual / 100);
+    }
+
+}
